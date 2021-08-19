47426059847409d5d7b945a5b005ca780573859d
victoru2 <89049935+victoru2@users.noreply.github.com>
2021-08-19 00:20:30 -0300

    modify arn rol

--- infrastructure/glue_job.tf
+++ infrastructure/glue_job.tf
@@ -1,6 +1,6 @@
 resource "aws_glue_job" "example" {
   name     = "desafio1-igti"
-  role_arn = "glue_full_access"
+  role_arn = "arn:aws:iam::451395640202:role/glue_full_access"
 
   command {
     script_location = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}/glue-code/pyspark/job_spark_from_tf.py"
