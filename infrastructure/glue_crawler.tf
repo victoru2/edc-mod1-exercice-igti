76f99c01e4cf7558b0b29f99a2e5d0e35f8c3bbb
victoru2 <89049935+victoru2@users.noreply.github.com>
2021-08-18 23:19:36 -0300

    modify policy

--- infrastructure/glue_crawler.tf
+++ infrastructure/glue_crawler.tf
@@ -1,11 +1,11 @@
 resource "aws_glue_catalog_database" "stream" {
-  name = "streamingdb"
+  name = "datalakeathena"
 }
 
 resource "aws_glue_crawler" "stream" {
   database_name = aws_glue_catalog_database.stream.name
   name          = "firehose_stream_s3_crawler"
-  role          = aws_iam_role.glue_role.arn
+  role          = "AWSGlueServiceRole-testegti"
 
   s3_target {
     path = "s3://${aws_s3_bucket.stream.bucket}/firehose/"
